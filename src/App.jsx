ef9463a6d147f9eeee57f341097e261ca7da056b
Raghavkapil <raghavkapil81@gmail.com>
2025-07-06 22:59:26 +0530

    pub_key issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ const currentUser = params.get('user') || 'guest';
 const otherUser = currentUser === 'user1' ? 'user2' : 'user1';
 
 const pubnub = new PubNub({
-  publishKey: 'pub-c-c17256b2-9dac-4f9b-9555-7d64f430922a',
-  subscribeKey: 'sub-c-0bb3ee19-8184-417c-8134-39cfe626653b',
+  publishKey: /*'pub-c-c17256b2-9dac-4f9b-9555-7d64f430922a'*/'',
+  subscribeKey: /*'sub-c-0bb3ee19-8184-417c-8134-39cfe626653b'*/'',
   uuid: currentUser,
 });
 
